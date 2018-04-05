b25f405f8c9b290ced62f1d0271c7f0c3b1c3ddb
Victor Truong <me@ifvictr.com>
2018-04-05 19:49:27 +0000

    Add Segment analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,15 @@ module.exports = {
         mapsApiKey: "AIzaSyBUq1lpcXDz3KGLcRBl6hhcxaFSY6wtBXo"
     },
     plugins: [
-        "gatsby-plugin-resolve-src",
         "gatsby-plugin-react-helmet",
         "gatsby-plugin-react-next",
+        "gatsby-plugin-resolve-src",
+        {
+            resolve: "gatsby-plugin-segment",
+            options: {
+                writeKey: "gsC4v5ma9Q9kISV9Iq57mFbpVetlLyLL"
+            }
+        },
         "gatsby-plugin-styled-components"
     ]
 };
