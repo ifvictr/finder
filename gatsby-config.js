e293f80ae74576946103becc991567c0b73a95af
Victor Truong <me@ifvictr.com>
2018-05-22 19:04:16 +0000

    Use new favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,24 @@ module.exports = {
         mapsApiKey: "AIzaSyBUq1lpcXDz3KGLcRBl6hhcxaFSY6wtBXo"
     },
     plugins: [
+        {
+            resolve: "gatsby-plugin-favicon",
+            options: {
+                logo: "./static/favicon.png",
+                injectHTML: true,
+                icons: {
+                    android: true,
+                    appleIcon: true,
+                    appleStartup: true,
+                    coast: false,
+                    favicons: true,
+                    firefox: true,
+                    twitter: false,
+                    yandex: false,
+                    windows: false
+                }
+            }
+        },
         "gatsby-plugin-react-helmet",
         "gatsby-plugin-react-next",
         "gatsby-plugin-resolve-src",
