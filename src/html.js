96f50afc87f993d63eb2ef206da6722c3a8b7355
Victor Truong <me@ifvictr.com>
2018-06-27 19:25:34 +0000

    Convert HTML to functional component

--- src/html.js
+++ src/html.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import data from "data.json";
 
 const isProduction = process.env.NODE_ENV === "production";
@@ -13,43 +13,38 @@ if(isProduction) {
     }
 }
 
-class HTML extends Component {
-    render() {
-        const {
-            htmlAttributes,
-            headComponents,
-            bodyAttributes,
-            preBodyComponents,
-            body,
-            postBodyComponents
-        } = this.props;
-        return (
-            <html {...htmlAttributes}>
-                <head>
-                    <meta charSet="utf-8" />
-                    <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-                    {headComponents}
-                    {isProduction && (
-                        <style
-                            id="gatsby-inlined-css"
-                            dangerouslySetInnerHTML={{ __html: stylesStr }}
-                        />
-                    )}
-                </head>
-                <body {...bodyAttributes}>
-                    {preBodyComponents}
-                    <div
-                        key="body"
-                        id="___gatsby"
-                        dangerouslySetInnerHTML={{ __html: body }}
-                    />
-                    {postBodyComponents}
-                    <script src={`https://maps.googleapis.com/maps/api/js?key=${data.googleMapsApiKey}&libraries=places`}></script>
-                </body>
-            </html>
-        );
-    }
-}
+const HTML = ({
+    htmlAttributes,
+    headComponents,
+    bodyAttributes,
+    preBodyComponents,
+    body,
+    postBodyComponents
+}) => (
+    <html {...htmlAttributes}>
+        <head>
+            <meta charSet="utf-8" />
+            <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+            {headComponents}
+            {isProduction && (
+                <style
+                    id="gatsby-inlined-css"
+                    dangerouslySetInnerHTML={{ __html: stylesStr }}
+                />
+            )}
+        </head>
+        <body {...bodyAttributes}>
+            {preBodyComponents}
+            <div
+                key="body"
+                id="___gatsby"
+                dangerouslySetInnerHTML={{ __html: body }}
+            />
+            {postBodyComponents}
+            <script src={`https://maps.googleapis.com/maps/api/js?key=${data.googleMapsApiKey}&libraries=places`}></script>
+        </body>
+    </html>
+);
 
 export default HTML;
