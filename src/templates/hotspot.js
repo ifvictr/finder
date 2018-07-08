2459d54cdb096cb248df481c836ff35041570283
Victor Truong <me@ifvictr.com>
2018-07-08 21:24:11 +0000

    Don't lazy-load hotspot pages

--- src/templates/hotspot.js
+++ src/templates/hotspot.js
@@ -1,6 +1,5 @@
 import React, { Fragment } from "react";
 import Helmet from "react-helmet";
-import LazyLoad from "react-lazyload";
 import { Container, Flex, Heading, theme } from "@hackclub/design-system";
 import ClubCard from "components/ClubCard";
 import Footer from "components/Footer";
@@ -13,13 +12,7 @@ const HotspotTemplate = ({ pathContext }) => (
         <Container align="center" color="black" px={3} w={1} style={{ display: "flex", flex: 1, flexDirection: "column", justifyContent: "center" }}>
             <Heading.h1 mt={4}>Hack Clubs in {pathContext.hotspot.name}</Heading.h1>
             <Flex justify="initial" py={4} style={{ margin: -theme.space[2] }} wrap>
-                {
-                    pathContext.clubs.map(club => (
-                        <LazyLoad key={club.id} height={0} offset={100} once overflow>
-                            <ClubCard data={club} />
-                        </LazyLoad>
-                    ))
-                }
+                {pathContext.clubs.map(club => <ClubCard key={club.id} data={club} />)}
             </Flex>
         </Container>
         <Footer />
