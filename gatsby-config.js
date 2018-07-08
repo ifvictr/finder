dd9a4940d3073883534d7e6c281c5d7b1d1d1bce
Victor Truong <me@ifvictr.com>
2018-07-08 21:34:55 +0000

    Remove trailing slashes

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,7 @@ module.exports = {
         },
         "gatsby-plugin-react-helmet",
         "gatsby-plugin-react-next",
+        "gatsby-plugin-remove-trailing-slashes",
         "gatsby-plugin-resolve-src",
         {
             resolve: "gatsby-plugin-segment",
